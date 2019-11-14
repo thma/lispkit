7802f1acc32f867f9c2a514cf4d3a733d62ed806
Thomas Mahler <thma32@web.de>
2019-11-14 21:51:22 +0100

    improve handling of prim ops

--- src/LispkitInterpreter.hs
+++ src/LispkitInterpreter.hs
@@ -16,9 +16,10 @@ eval (SList [op@(SAtom opName), x, y]) env =
     Just fun -> fun (eval x env) (eval y env)
     Nothing  -> apply (eval op env) (eval x env) (eval y env)
 
-eval (SList [op@(SAtom opName), x]) env
-  | isUnaryOp op = unaryOp opName (eval x env)
-  | otherwise    = apply (eval op env) (eval x env)
+eval (SList [op@(SAtom opName), x]) env =
+  case unaryOp opName of
+    Just fun -> fun (eval x env)
+    Nothing  -> apply (eval op env) (eval x env)
 
 eval x _ = SError $ "No rule for evaluating " ++ show x
 
@@ -40,17 +41,14 @@ binOpCons :: SExpr -> SExpr -> SExpr
 binOpCons hd (SList tl) = SList (hd:tl)
 binOpCons hd tl         = SList [hd, tl]
 
-
-isUnaryOp :: SExpr -> Bool
-isUnaryOp (SAtom op) = op `elem` ["car", "cdr", "caar", "cadr", "cdar", "cddr"]
-
-unaryOp :: String -> SExpr -> SExpr
-unaryOp "car" = opCar
-unaryOp "cdr" = opCdr
-unaryOp "cadr" = opCar . opCdr
-unaryOp "caar" = opCar . opCar
-unaryOp "cdar" = opCdr . opCar
-unaryOp "cddr" = opCdr . opCdr
+unaryOp :: String -> Maybe (SExpr -> SExpr)
+unaryOp "car"  = Just opCar
+unaryOp "cdr"  = Just opCdr
+unaryOp "cadr" = Just $ opCar . opCdr
+unaryOp "caar" = Just $ opCar . opCar
+unaryOp "cdar" = Just $ opCdr . opCar
+unaryOp "cddr" = Just $ opCdr . opCdr
+unaryOp _      = Nothing
 
 opCar :: SExpr -> SExpr
 opCar (SList (hd:_)) = hd
