b28bd6f21b40f7f58711c8f6b0e35393d7acb756
Mahler, Thomas <thomas.mahler@ista.com>
2019-11-18 16:08:26 +0100

    improve abstracting variables

--- src/LambdaCompiler.hs
+++ src/LambdaCompiler.hs
@@ -17,9 +17,12 @@ data CompileError = CompileError
 
 -- | 
 data LTerm = LInt Integer
+           | LBool Bool
            | LVar String
            | LBinPrimOp String LTerm LTerm
-           | LUnyPrimOp String LTerm LTerm
+           | LBinOp String LTerm LTerm
+           | LUnyPrimOp String LTerm
+           | LUnyOp String LTerm
            | LApp LTerm LTerm
            | LAbs String LTerm
              deriving (Show, Eq)
@@ -27,23 +30,36 @@ data LTerm = LInt Integer
 -- | a lambda term from a lisp symbolic expression
 parseTerm :: (MonadError CompileError m) => SExpr -> m LTerm
 parseTerm (SAtom v) = return $ LVar v
-parseTerm (SInt n) = return $ LInt n
---parseTerm (SList [SAtom "lambda", SList vars, t]) = do
-parseTerm (SList [SAtom "lambda", SList [SAtom var], t]) = do
+parseTerm (SInt n)  = return $ LInt n
+parseTerm (SBool b) = return $ LBool b
+parseTerm (SList [SAtom "lambda", SList vars, t]) = do
   t' <- parseTerm t
-  return $ LAbs var t'
+  return $ abstractVars vars t'
+    where
+      abstractVars ([SAtom var])      term = LAbs var term
+      abstractVars ((SAtom var):rest) term = LAbs var (abstractVars rest term)
+
 parseTerm (SList [SAtom fun, t1, t2]) = do
   t1' <- parseTerm t1
   t2' <- parseTerm t2
   case binOp fun of
     Just op -> return $ LBinPrimOp fun t1' t2'
-    Nothing  -> return $ LBinPrimOp (fun ++ ": not found") t1' t2' --apply (eval op env) (SList [eval x env, eval y env]) env
+    Nothing -> return $ LBinOp fun t1' t2'
+
+parseTerm (SList [SAtom fun, t1]) = do
+  t1' <- parseTerm t1
+  case unaryOp fun of
+    Just op -> return $ LUnyPrimOp fun t1'
+    Nothing -> return $ LUnyOp fun t1'
 
 parseTerm (SList [t1, t2]) = do
   t1' <- parseTerm t1
   t2' <- parseTerm t2
   return $ LApp t1' t2'
+
 parseTerm _ = throwError CompileError
+
+
 
 -- | Compile the given lisp code to lambda terms
 compile :: String -> Either CompileError LTerm
