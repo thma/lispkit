f63ea2efd7b2128aed4fe0edb7be20bab98a92f7
Mahler, Thomas <thomas.mahler@ista.com>
2019-12-03 15:52:27 +0100

    implement quote, lambda and if

--- src/LambdaInterpreter.hs
+++ src/LambdaInterpreter.hs
@@ -16,11 +16,18 @@ type EvalErrorMonad = Either EvalError
 eval :: LTerm -> Environment -> EvalErrorMonad LTerm
 eval num@(LInt _) _   = return num
 eval bool@(LBool _) _ = return bool
-eval exp@(LVar name) env =
-  case lookup name env of
-    Just val -> return val
-    Nothing ->  if isPrimOp name
-        then return exp
-        else throwError (EvalError $ name ++ " not found")
+eval exp@(LVar name) env = case lookup name env of
+  Just val -> return val
+  Nothing  -> if isPrimOp name
+                then return exp
+                else throwError (EvalError $ name ++ " not found")
+eval (LUnyOp "quote" expr) _ = return expr
+eval lambda@(LAbs v body) _  = return lambda
+eval (LApp (LVar "if") [test, thenPart, elsePart]) env = do 
+  evalTest <- eval test env
+  case evalTest of
+    (LBool True)  -> eval thenPart env
+    (LBool False) -> eval elsePart env
+
 
 eval term _ = throwError (EvalError $ "can't evaluate " ++ show term)
